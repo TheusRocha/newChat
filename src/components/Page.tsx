7787335c7f90f7a2d3fe9b0001be193e018189dd
matheus.dsn.cir <matheus.dsn.cir@alterdata.com.br>
2021-11-10 19:49:09 -0300

    adjust

--- src/components/Page.tsx
+++ src/components/Page.tsx
@@ -2,7 +2,8 @@ import styled from 'styles'
 
 export const Page = styled.main`
   display: flex;
-  background-color: ${({ theme }) => theme.colors.background0};
+  background-color: ${({ theme }) => theme.colors.gray[900]};
+  color: ${({ theme }) => theme.colors.gray[200]};
   width: 100vw;
   height: 100vh;
 `
--- src/styles/global.ts
+++ src/styles/global.ts
@@ -16,7 +16,6 @@ const GlobalStyles = createGlobalStyle`
   }
 
   body {
-    color: ${({ theme }) => theme.colors.font0};
     font-family: -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen, Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif
   }
 `
